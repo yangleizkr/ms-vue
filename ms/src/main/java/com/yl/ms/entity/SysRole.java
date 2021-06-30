cec6ed17bfc770b89b4ab46f52de913897343e36
杨磊 <52030858+yangleizkr@users.noreply.github.com>
2021-06-30 17:17:04 +0800

    提交

--- ms/src/main/java/com/yl/ms/entity/SysRole.java
+++ ms/src/main/java/com/yl/ms/entity/SysRole.java
@@ -1,23 +1,54 @@
 package com.yl.ms.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Set;
+import java.util.List;
 
-/**
- * Created by yl on 2021/3/17
- */
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
+@TableName(value = "sys_role")
 public class SysRole {
+    @TableId(value = "id", type = IdType.INPUT)
+    private Integer id;
 
-    private String id;
+    @TableField(value = "role_code")
+    private String roleCode;
+
+    @TableField(value = "role_name")
     private String roleName;
-    /**
-     * 角色对应权限集合
-     */
-    private Set<Permissions> permissions;
+
+    @TableField(exist = false)
+    private List<SysMenu> sysMenuList;
+
+    @TableField(exist = false)
+    private List<SysPermission> sysPermissionList;
+
+    public List<SysMenu> getSysMenuList() {
+        return sysMenuList;
+    }
+
+    public void setSysMenuList(List<SysMenu> sysMenuList) {
+        this.sysMenuList = sysMenuList;
+    }
+
+    public List<SysPermission> getSysPermissionList() {
+        return sysPermissionList;
+    }
+
+    public void setSysPermissionList(List<SysPermission> sysPermissionList) {
+        this.sysPermissionList = sysPermissionList;
+    }
+
+    public static final String COL_ID = "id";
+
+    public static final String COL_ROLE_CODE = "role_code";
+
+    public static final String COL_ROLE_NAME = "role_name";
 }
